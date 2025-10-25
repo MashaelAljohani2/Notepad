e731943404c94de2dcdc718a1130551ea33e3662
Mashael <MashaelAljohani9@gmail.com>
2025-10-25 22:13:31 +0300

    Save local changes before rebase

---
+++ build/Desktop-Debug/ui_mainwindow.h
@@ -0,0 +1,107 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.13
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Notepad
+{
+public:
+    QAction *actionNew;
+    QAction *actionOpen;
+    QAction *actionSave;
+    QAction *actionSave_as;
+    QAction *actionExit;
+    QWidget *centralwidget;
+    QTextEdit *textEdit;
+    QMenuBar *menubar;
+    QMenu *menuFile;
+    QStatusBar *statusbar;
+    QToolBar *toolBar;
+
+    void setupUi(QMainWindow *Notepad)
+    {
+        if (Notepad->objectName().isEmpty())
+            Notepad->setObjectName(QString::fromUtf8("Notepad"));
+        Notepad->resize(522, 546);
+        actionNew = new QAction(Notepad);
+        actionNew->setObjectName(QString::fromUtf8("actionNew"));
+        actionOpen = new QAction(Notepad);
+        actionOpen->setObjectName(QString::fromUtf8("actionOpen"));
+        actionSave = new QAction(Notepad);
+        actionSave->setObjectName(QString::fromUtf8("actionSave"));
+        actionSave_as = new QAction(Notepad);
+        actionSave_as->setObjectName(QString::fromUtf8("actionSave_as"));
+        actionExit = new QAction(Notepad);
+        actionExit->setObjectName(QString::fromUtf8("actionExit"));
+        centralwidget = new QWidget(Notepad);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        textEdit = new QTextEdit(centralwidget);
+        textEdit->setObjectName(QString::fromUtf8("textEdit"));
+        textEdit->setGeometry(QRect(0, 0, 521, 481));
+        Notepad->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(Notepad);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 522, 23));
+        menuFile = new QMenu(menubar);
+        menuFile->setObjectName(QString::fromUtf8("menuFile"));
+        Notepad->setMenuBar(menubar);
+        statusbar = new QStatusBar(Notepad);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        Notepad->setStatusBar(statusbar);
+        toolBar = new QToolBar(Notepad);
+        toolBar->setObjectName(QString::fromUtf8("toolBar"));
+        Notepad->addToolBar(Qt::TopToolBarArea, toolBar);
+
+        menubar->addAction(menuFile->menuAction());
+        menuFile->addSeparator();
+        menuFile->addAction(actionNew);
+        menuFile->addAction(actionOpen);
+        menuFile->addAction(actionSave);
+        menuFile->addAction(actionSave_as);
+        menuFile->addAction(actionExit);
+
+        retranslateUi(Notepad);
+
+        QMetaObject::connectSlotsByName(Notepad);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *Notepad)
+    {
+        Notepad->setWindowTitle(QCoreApplication::translate("Notepad", "MainWindow", nullptr));
+        actionNew->setText(QCoreApplication::translate("Notepad", "New", nullptr));
+        actionOpen->setText(QCoreApplication::translate("Notepad", "Open", nullptr));
+        actionSave->setText(QCoreApplication::translate("Notepad", "Save", nullptr));
+        actionSave_as->setText(QCoreApplication::translate("Notepad", "Save as", nullptr));
+        actionExit->setText(QCoreApplication::translate("Notepad", "Exit", nullptr));
+        menuFile->setTitle(QCoreApplication::translate("Notepad", "File", nullptr));
+        toolBar->setWindowTitle(QCoreApplication::translate("Notepad", "toolBar", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Notepad: public Ui_Notepad {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
